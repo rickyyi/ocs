4e590c97ffc4e34dcd2c3624c3fab543d67c4b8a
rickyyi <845477519@qq.com>
2018-08-04 17:52:20 +0800

    add log

--- src/main/java/cn/hf/manage/service/impl/AnswerEslEventHandler.java
+++ src/main/java/cn/hf/manage/service/impl/AnswerEslEventHandler.java
@@ -2,6 +2,7 @@ package cn.hf.manage.service.impl;
 
 import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.freeswitch.esl.client.transport.event.EslEvent;
 
 import cn.hf.manage.service.EslEventHandler;
@@ -12,9 +13,12 @@ import cn.hf.manage.util.OutBoundInstance;
  */
 public class AnswerEslEventHandler implements EslEventHandler {
 
+	private static Logger log = Logger.getLogger(AnswerEslEventHandler.class);
+	
     @Override
 	public void handle(EslEvent eslEvent) {
     	String uuid = eslEvent.getEventHeaders().get("Channel-Call-UUID");
+    	log.info("接听事件:" + uuid);
     	if (OutBoundInstance.getOutBound(uuid) == null) {
         	return;
         }
--- src/main/java/cn/hf/manage/service/impl/HangupEslEventHandler.java
+++ src/main/java/cn/hf/manage/service/impl/HangupEslEventHandler.java
@@ -2,6 +2,7 @@ package cn.hf.manage.service.impl;
 
 import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.freeswitch.esl.client.transport.event.EslEvent;
 
 import cn.hf.manage.service.EslEventHandler;
@@ -11,9 +12,13 @@ import cn.hf.manage.util.OutBoundInstance;
  * Created by cookie on 2018/4/10 0010.
  */
 public class HangupEslEventHandler implements EslEventHandler{
-    @Override
+    
+	private static Logger log = Logger.getLogger(HangupEslEventHandler.class);
+	
+	@Override
 	public void handle(EslEvent eslEvent) {
         String uuid = eslEvent.getEventHeaders().get("Channel-Call-UUID");
+        log.info("挂断事件:" + uuid);
         if (OutBoundInstance.getOutBound(uuid) == null) {
         	return;
         }
